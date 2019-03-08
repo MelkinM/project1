65fa1a90de01ab74bc2fc0d4f98ca125d585f03e
MelkinM <48342559+MelkinM@users.noreply.github.com>
2019-03-08 09:58:08 +0530

    Add files via upload

---
+++ binary.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+#include<conio.h>
+#include<time.h>
+void merge(int a[],int low,int mid,int high)
+{
+    int i,j,k,b[20];
+    i=low,j=mid+1,k=low;
+    while(i<=mid&&j<=high)
+    {
+        if(a[i]<=a[i])
+            b[k++]=a[i++];
+        else
+            b[k++]=a[j++];
+    }
+    while(i<=mid)
+        b[k++]=a[i++];
+    while(j<=high)
+        b[k++]=a[j++];
+    for(k=low;k<=high;k++)
+        a[k]=b[k];
+}
+void mergesort(int a[],int low,int high)
+{
+    int mid;
+    if(low>=high)
+        return;
+    mid=(low+high)/2;
+    mergesort(a,low,mid);
+    mergesort(a,mid+1,high);
+    merge(a,low,mid,high);
+}
+void main()
+{
+int n,a[2000],k,l;
+clock_t st ,et;
+double ts;
+printf("\n enter how many numbers:");
+scanf("%d",&n);
+printf("\n the random numbers are:\n");
+for(k=1;k<n;k++)
+{
+
+    a[k]=rand();
+    printf("%d\t",&a[k]);
+}
+st=clock();
+mergesort(a,l,n);
+et=clock();
+ts=(double)(et-st)/CLOCKS_PER_SEC;
+printf("\n sorted numbers are:\n");
+for(k=1;k<=n;k++)
+    printf("%d\n",&a[k]);
+printf("\n the time taken is %e",ts);
+getch();
+}
+
+
+
+
+
+
+
